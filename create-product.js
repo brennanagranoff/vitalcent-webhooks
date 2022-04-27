7567c35416e1a4db0760653701a684997a78355e
brennanagranoff <brennanagranoff74@gmail.com>
2022-04-26 23:51:24 -0700

    upload script

--- create-product.js
+++ create-product.js
@@ -9,7 +9,7 @@ var WooCommerce = new WooCommerceAPI({
   consumerKey: "ck_a57df1fb4a7f64a22e05b3f25ea6da01617ee027",
   consumerSecret: "cs_73a6a650233a895afb0b8ade7f516f1e4f8a13ba",
   wpAPI: true,
-  version: "wc/v1",
+  version: "wc/v3",
 });
 
 async function main() {
@@ -34,114 +34,166 @@ async function main() {
     if (currentRow["product-type"] == "parent-variable") {
       var product = { name: currentRow["name"], sku: currentRow["sku"], type: "variable" };
       productsToUpload.push(product);
-      var productAttributes = {}
-        productAttributes[currentRow["sku"]] = {};
-  productAttributes[currentRow["sku"]].names = []
-  productAttributes[currentRow["sku"]].uniqueNames = []
-  productAttributes[currentRow["sku"]].values = []
-  productAttributes[currentRow["sku"]].uniqueValues = []
+      var productAttributes = {};
+      productAttributes[currentRow["sku"]] = {};
+      productAttributes[currentRow["sku"]].names = [];
+      productAttributes[currentRow["sku"]].uniqueNames = [];
+      productAttributes[currentRow["sku"]].values = [];
+      productAttributes[currentRow["sku"]].uniqueValues = [];
     }
   }
 
   //VARIANT Attributes LOOP
   //This generates an array of all possible attributes and values
-  var allAttributes = []
+  var allAttributes = [];
   for (var i = 0; i < productData.length; i++) {
     var currentRow = productData[i];
 
     //go through attrbutes
     if (currentRow["product-type"] == "child") {
-      for (key in currentRow){
+      variantsToUpload.push(currentRow);
+      for (key in currentRow) {
         //skip if not an attribute column
-        if((key.includes("attribute") && key.includes("name")) || !key.includes("attribute")){
-          continue
+        if ((key.includes("attribute") && key.includes("name")) || !key.includes("attribute")) {
+          continue;
         }
         //get attribute number
-        var attributeNumber = key.split("-")
+        var attributeNumber = key.split("-");
         //grab attribute number
-        attributeNumber = (attributeNumber[1])
+        attributeNumber = attributeNumber[1];
         //skip if either is blank
-        if (currentRow[`attribute-${attributeNumber}-name`].length < 1 || currentRow[`attribute-${attributeNumber}-value`].length < 1){
-          continue
+        if (currentRow[`attribute-${attributeNumber}-name`].length < 1 || currentRow[`attribute-${attributeNumber}-value`].length < 1) {
+          continue;
         }
-        
+
         allAttributes.push({
           sku: currentRow["sku"],
           name: currentRow[`attribute-${attributeNumber}-name`],
           parentSku: currentRow["parent-sku"],
-          value: currentRow[`attribute-${attributeNumber}-value`]
-        })
-        
+          value: currentRow[`attribute-${attributeNumber}-value`],
+        });
       }
     }
   }
 
+  //Merge Attributes Together By Parent Product
+  //We find all attributes and possible values and group to their parent
+  for (var i = 0; i < allAttributes.length; i++) {
+    var currentAttribute = allAttributes[i];
+    //check if parent sku exists
+    var parentSkuExists = false;
 
-//Merge Attributes Together By Parent Product
-//We find all attributes and possible values and group to their parent
-for (var i=0;i<allAttributes.length;i++){
-  var currentAttribute = allAttributes[i]
-  //check if parent sku exists
-  var parentSkuExists = false;
+    //loop over parent products array to see if it exists
+    for (var s = 0; s < productsToUpload.length; s++) {
+      var currentProduct = productsToUpload[s];
+      if (currentProduct.sku == currentAttribute.parentSku) {
+        parentSkuExists = true;
+        break;
+      }
+    }
 
-  //loop over parent products array to see if it exists
-  for(var s=0;s<productsToUpload.length;s++){
-    var currentProduct = productsToUpload[s]
-    if (currentProduct.sku == currentAttribute.parentSku){
-      parentSkuExists = true;
-      break;
+    //skip if no parent product in upload
+    if (!parentSkuExists) {
+      continue;
+    }
+
+    productAttributes[currentAttribute.parentSku].names.push(currentAttribute.name);
+    productAttributes[currentAttribute.parentSku].values.push({
+      attributeName: currentAttribute.name,
+      value: currentAttribute.value,
+    });
+  }
+  for (key in productAttributes) {
+    //get unique attributes name and values
+    var currentProduct = productAttributes[key];
+    productAttributes[key].attributeData = [];
+    var names = currentProduct.names;
+    productAttributes[key].uniqueNames = names.filter((v, i, a) => a.indexOf(v) === i);
+    for (var s = 0; s < productAttributes[key].uniqueNames.length; s++) {
+      productAttributes[key].attributeData.push({
+        name: productAttributes[key].uniqueNames[s],
+        visible: false,
+        variation: true,
+        options: [],
+      });
+    }
+    var values = currentProduct.values;
+    productAttributes[key].uniqueValues = values.filter((v, i, a) => a.findIndex((v2) => JSON.stringify(v2) === JSON.stringify(v)) === i);
+    for (var s = 0; s < productAttributes[key].uniqueValues.length; s++) {
+      var attributeObjectIndex = productAttributes[key].attributeData.findIndex((object) => {
+        return object.name === productAttributes[key].uniqueValues[s].attributeName;
+      });
+      productAttributes[key].attributeData[attributeObjectIndex].options.push(productAttributes[key].uniqueValues[s].value);
+    }
+
+    for (var s = 0; s < productAttributes[key].attributeData.length; s++) {
+      var currentAttribute = productAttributes[key].attributeData[s];
+      if (currentAttribute.options.length < 2) {
+        currentAttribute.visible = true;
+        currentAttribute.variation = false;
+      }
+      productAttributes[key].attributeData[s] = currentAttribute;
+    }
+
+    delete productAttributes[key].uniqueValues;
+    delete productAttributes[key].values;
+    delete productAttributes[key].uniqueNames;
+    delete productAttributes[key].names;
+  }
+
+  //CREATE PARENT PRODUCTS
+  for (var i = 0; i < productsToUpload.length; i++) {
+    productsToUpload[i].attributes = productAttributes[productsToUpload[i].sku].attributeData;
+    var createProduct = await WooCommerce.postAsync("products", productsToUpload[i]);
+    if (createProduct.statusCode == "201") {
+      console.log(`Successfully created SKU: ${productsToUpload[i].sku}`);
+    } else {
+      console.log(`Error creating SKU: ${productsToUpload[i].sku} --- Error ${createProduct.body}`);
     }
   }
 
-  //skip if no parent product in upload
-  if (!parentSkuExists){
-    continue
-  }
-
-  productAttributes[currentAttribute.parentSku].names.push(currentAttribute.name)
-  productAttributes[currentAttribute.parentSku].values.push({
-    attributeName: currentAttribute.name,
-    value: currentAttribute.value
-  })
-
-}
-for (key in productAttributes){
-  //get unique attributes name and values
-  var currentProduct = productAttributes[key]
-  productAttributes[key].attributeData = [];
-  var names = currentProduct.names;
-  productAttributes[key].uniqueNames = names.filter((v, i, a) => a.indexOf(v) === i);
-  for (var s=0;s<productAttributes[key].uniqueNames.length;s++){
-    productAttributes[key].attributeData.push({
-      name: productAttributes[key].uniqueNames[s],
-      visible: false,
-      variation:true,
-      options:[]
-    })
-  }
-  var values = currentProduct.values;
-  productAttributes[key].uniqueValues = values.filter((v,i,a)=>a.findIndex(v2=>(JSON.stringify(v2) === JSON.stringify(v)))===i)
-  for (var s=0;s<productAttributes[key].uniqueValues.length;s++){
-    var attributeObjectIndex = productAttributes[key].attributeData.findIndex(object => {return object.name === productAttributes[key].uniqueValues[s].attributeName;});
-    (productAttributes[key].attributeData[attributeObjectIndex].options.push(productAttributes[key].uniqueValues[s].value))
-  }
-
-  for (var s=0;s<productAttributes[key].attributeData.length;s++){
-    var currentAttribute = productAttributes[key].attributeData[s];
-    if (currentAttribute.options.length < 2){
-      currentAttribute.visible = true;
-      currentAttribute.variation = false;
+  //CREATE VARIANT PRODUCTS
+  for (var i = 0; i < variantsToUpload.length; i++) {
+    currentRow = variantsToUpload[i];
+    variantData = {
+      regular_price: "1.99",
+      sku: currentRow["sku"],
+      attributes: [],
+    };
+    var parentProduct = await WooCommerce.getAsync(`products?sku=${currentRow["parent-sku"]}`);
+    parentProduct = JSON.parse(parentProduct.body);
+    if (parentProduct.length != 1) {
+      console.log(`FAILED: Couldn't locate parent product for SKU: ${currentRow["parent-sku"]}`);
+      continue;
     }
-    productAttributes[key].attributeData[s] = currentAttribute
+
+    parentProduct = parentProduct[0];
+
+    for (key in currentRow) {
+      if ((key.includes("attribute") && key.includes("name")) || !key.includes("attribute")) {
+        continue;
+      }
+
+      //get attribute number
+      var attributeNumber = key.split("-");
+      //grab attribute number
+      attributeNumber = attributeNumber[1];
+      //skip if either is blank
+      if (currentRow[`attribute-${attributeNumber}-name`].length < 1 || currentRow[`attribute-${attributeNumber}-value`].length < 1) {
+        continue;
+      }
+
+      variantData.attributes.push({
+        name: currentRow[`attribute-${attributeNumber}-name`],
+        option: currentRow[`attribute-${attributeNumber}-value`],
+      });
+    }
+    var createVariant = await WooCommerce.postAsync(`products/${parentProduct.id}/variations`, variantData);
+    if (createVariant.statusCode == "201") {
+      console.log(`Successfully created variant: ${variantData.sku}`);
+    } else {
+      console.log(`Error creating variant: ${variantData.sku} --- Error ${createVariant.body}`);
+    }
   }
-
-  delete productAttributes[key].uniqueValues
-  delete productAttributes[key].values
-  delete productAttributes[key].uniqueNames
-  delete productAttributes[key].names
-
-
-}
-console.log(productAttributes["GLOVES-3"])
 }
 main();
